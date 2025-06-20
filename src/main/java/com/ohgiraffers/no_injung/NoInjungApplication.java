c4e18400cfcf0a3335bde1bb22e201e1bf0b7270
한솔 <hs543678@gmail.com>
2025-06-20 16:42:52 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/no_injung/NoInjungApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.no_injung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoInjungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoInjungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/no_injung/NoInjungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.no_injung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoInjungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
